e66fc535d065bc91466c680d41d4dbdddcd4a54f
xingxuewang <945506440@qq.com>
2020-08-08 12:30:57 +0800

    文章编辑

--- src/views/Article/edit.js
+++ src/views/Article/edit.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react'
-
+import {Card,Form, Input, Button } from 'antd';
 export default class Edit extends Component {
+    onFinish = values => {
+        console.log('Success:', values);
+    };
     render() {
         return (
-            <div>
-                文章编辑
-            </div>
+          <Card size="small" title="文章编辑" extra={<Button>取消</Button>}>
+              <Form
+                onFinish={this.onFinish}
+                >
+                <Form.Item
+                    name="title"
+                    rules={[
+                        {
+                            required: true,
+                            message: '请输入标题',
+                        },
+                        {
+                            max: 8,
+                            message: '最长8位数',
+                        },
+                        {
+                            min:4,
+                            message: '最短4位数',
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary" htmlType="submit">
+                    Submit
+                    </Button>
+                </Form.Item>
+                </Form>
+          </Card>
         )
     }
 }
