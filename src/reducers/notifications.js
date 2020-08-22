ea706a6a81f10cf26b4a86ea3168dc881ae8a0ba
xingxuewang <945506440@qq.com>
2020-08-22 21:09:02 +0800

    通知中心

--- src/reducers/notifications.js
+++ src/reducers/notifications.js
@@ -1,42 +1,61 @@
 import actionType from '../actions/actionType'
 
-const initState ={
-    isLoading:false,
-    list:[{
-        id:1,
-        title:'标题',
-        desc:'我是描述哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈',
-        hasRead:false
-    },
-    {
-        id:2,
-        title:'标题2',
-        desc:'速度大幅度顶顶顶顶顶顶顶顶顶顶',
-        hasRead:true
-    }]
+const initState = {
+    isLoading: false,
+    list: [{
+            id: 1,
+            title: '标题',
+            desc: '我是描述哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈',
+            hasRead: false
+        },
+        {
+            id: 2,
+            title: '标题2',
+            desc: '速度大幅度顶顶顶顶顶顶顶顶顶顶',
+            hasRead: true
+        },
+        {
+            id: 3,
+            title: '标题',
+            desc: '我是描述哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈',
+            hasRead: false
+        }, {
+            id: 4,
+            title: '标题',
+            desc: '我是描述哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈',
+            hasRead: false
+        }, {
+            id: 5,
+            title: '标题',
+            desc: '我是描述哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈',
+            hasRead: false
+        },
+    ]
 }
-export default(state = initState,action)=>{
-   switch(action.type){
-       case actionType.MARK_NOTIFICATIONS_BY_ID:
-           const {list,other} = state;
-           return {
-            ...other,
-            list:list.map(item=>{
-                if(item.id === action.payload.id){
-                    item.hasRead = true;
-                }
-                return item
-            })
-          }
-          case actionType.MARK_NOTIFICATIONS_BY_ALL:
+export default (state = initState, action) => {
+    switch (action.type) {
+        case actionType.MARK_NOTIFICATIONS_BY_ID:
+            const {
+                list, other
+            } = state;
             return {
-             ...state,
-             list:state.list.map(item=>{
-                 item.hasRead = true;
-                 return item
-             })
-           }
-            default:
-                return state
-   }
+                ...other,
+                list: list.map(item => {
+                    if (item.id === action.payload.id) {
+                        item.hasRead = true;
+                    }
+                    return item
+                })
+            }
+            case actionType.MARK_NOTIFICATIONS_BY_ALL:
+                return {
+                    ...state,
+                    list: state.list.map(item => {
+                        item.hasRead = true;
+                        return item
+                    })
+                }
+                default:
+                    return state
+    }
 }
