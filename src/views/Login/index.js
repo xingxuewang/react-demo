6de7057b4bb12b45b92f48df5daee643348ca57c
xingxuewang <945506440@qq.com>
2020-08-24 22:14:22 +0800

    登录

--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -1,11 +1,62 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { Form, Input, Button } from 'antd';
+import './index.css'
+const layout = {
+  labelCol: {
+    span: 5,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 5,
+    span: 16,
+  },
+};
 
-export default class index extends Component {
-    render() {
-        return (
-            <div>
-                登录
-            </div>
-        )
-    }
-}
+const Demo = () => {
+  const onFinish = values => {
+    console.log('Success:', values);
+  };
+  return (
+    <Form
+      {...layout}
+      onFinish={onFinish}
+      className='loginFrom'
+    >
+      <Form.Item
+        label="用户名"
+        name="username"
+        rules={[
+          {
+            required: true,
+            message: '请输入用户名!',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="密码"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: '请输入密码!',
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
+export default Demo
