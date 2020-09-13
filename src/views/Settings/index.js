e99e4f675692c130bd0120c206c4d12481b2221f
xingxuewang <945506440@qq.com>
2020-09-13 13:27:17 +0800

    mobx

--- src/views/Settings/index.js
+++ src/views/Settings/index.js
@@ -6,6 +6,7 @@ class Index extends Component {
     render() {
         return (
             <div>
+                <p>使用mobx</p>
                 <button onClick={Counter.decrement}>-</button>
                 {Counter.count}
                 <button onClick={Counter.increment}>+</button>
